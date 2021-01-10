96fc5e71619b23e7a2c67bce2b4985274127e480
double-slash-tt <double.slash.thunder@gmail.com>
2021-01-10 23:33:58 +0900

    基礎コンポーネント

---
+++ Runtime/SFX/Core/SFXGun/SFXGun.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SoundShooter.SFX
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SFXGun : SFXWeapon
+    {
+        //=================================
+        // SerializeField
+        //=================================
+        [SerializeField] private SFXBarrel m_barrel = default;
+        [SerializeField] private SFXMagazine m_magazine = default;
+        [SerializeField] private SFXGrip m_grip = default;
+
+        //=================================
+        // Method
+        //=================================
+    }
+}
---
+++ Runtime/SFX/Impl/SFXAudioClip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SoundShooter.SFX
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SFXAudioClip : SFXAmmo
+    {
+        //===============================
+        // SerializeField
+        //===============================
+        [SerializeField] private AudioClip m_audioClip = default;
+
+        //===============================
+        // Method
+        //===============================
+    }
+}
